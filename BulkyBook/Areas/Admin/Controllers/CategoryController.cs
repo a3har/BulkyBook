8c81d1c3466e2c487a505e8eefbb245acc238bcf
877597 <aws.azharmuham@gmail.com>
2020-12-10 22:11:57 +0530

    Removed all View Templates, created Fresh Index View

---
+++ BulkyBook/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using BulkyBook.DataAccess.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var AllObjs = _unitOfWork.Category.GetAll();
+            return Json(new { data = AllObjs });
+        }
+
+        #endregion
+    }
+}
