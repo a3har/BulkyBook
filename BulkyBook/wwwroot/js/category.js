589a6991ce527343bb5655d4a1e798ce31993a6a
azhar_laptop <aws.azharmuham@gmail.com>
2020-12-10 22:47:41 +0530

    Fixed Minor Bugs

---
+++ BulkyBook/wwwroot/js/category.js
@@ -0,0 +1,28 @@
+﻿var dataTable;
+
+$('document').ready(function () {
+    loadDataTable();
+})
+
+function loadDataTable() {
+    datatable = $('#tblData').DataTable({
+                "ajax": "/Admin/Category/GetAll",
+                "columns": [{ "data": "name", "width": "60%" },
+                    {
+                        "data": "id",
+                        "render": function (data) {
+                            return `
+                                    <div class="text-center">
+                                        <a href="/Admin/Category/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
+                                           <i class="fas fa-edit"></i>
+                                        </a>
+                                        <a class="btn btn-danger text-white" style="cursor:pointer">
+                                            <i class="fas fa-trash-alt"></i>
+                                        </a>
+                                    </div>
+                                    `
+                        }
+                    }
+                ]
+            })
+}
