fab1625c443fc33ed128ee6147034fdfe86789e8
877597 <aws.azharmuham@gmail.com>
2020-12-10 14:56:21 +0530

    Generated Repository and CategoryRepository

---
+++ BulkyBook.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base (db)
+        {
+            _db = db;
+        }
+        public void Update(Category category)
+        {
+            var ObjFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if (ObjFromDb != null)
+            {
+                ObjFromDb.Name = category.Name;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
