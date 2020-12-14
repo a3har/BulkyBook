51f1ed14ba76b54e3316fb7d19db0ae633585623
azhar_laptop <aws.azharmuham@gmail.com>
2020-12-14 11:12:57 +0530

    Changed covertypejs to categoryjs in Index of Catgory

--- BulkyBook/wwwroot/js/product.js
+++ BulkyBook/wwwroot/js/product.js
@@ -6,12 +6,12 @@ $('document').ready(function () {
 
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
-                "ajax": "/Admin/Category/GetAll",
+                "ajax": "/Admin/Product/GetAll",
         "columns": [{ "data": "title", "width": "15%" },
             { "data": "isbn", "width": "15%" },
             { "data": "price", "width": "15%" },
             { "data": "author", "width": "15%" },
-            { "data": "category.Name", "width": "15%" },
+            { "data": "category.name", "width": "15%" },
                     {
                         "data": "id",
                         "render": function (data) {
